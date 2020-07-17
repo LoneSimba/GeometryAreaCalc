540b11728ef511f185785f49d36f4abbda5ba507
Vladimir <siluet-stalker99@yandex.ru>
2020-07-17 22:49:17 +0300

    Добавлены решения задач для MSSQL

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿Select ([item_name]), coalesce(([cat_name]), 'no cat') as cat_name 
+from [Items] 
+left join [item_cats] on [Items].[item_id]=[item_cats].[item_id] 
+left join [Cats] on [item_cats].[cat_id]=[Cats].[cat_id]
---
+++ SQL/tables.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Cats] (
+    [cat_id]   INT        IDENTITY (1, 1) NOT NULL,
+    [cat_name] NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([cat_id] ASC)
+);
+  
+ GO
+
+CREATE TABLE [dbo].[Items] (
+    [item_id]   INT        IDENTITY (1, 1) NOT NULL,
+    [item_name] NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([item_id] ASC)
+);
+
+GO
+
+CREATE TABLE [dbo].[item_cats] (
+    [Id]      INT IDENTITY (1, 1) NOT NULL,
+    [cat_id]  INT NULL,
+    [item_id] INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_item_cats_cats] FOREIGN KEY ([cat_id]) REFERENCES [dbo].[Cats] ([cat_id]),
+    CONSTRAINT [FK_item_cats_items] FOREIGN KEY ([item_id]) REFERENCES [dbo].[Items] ([item_id])
+);
+
